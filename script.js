83bf9125e0e888334349826e76ffb8ad2516870d
yoursocialmd <yoursocialmd@gmail.com>
2022-03-03 17:56:04 +0530

    changed the text and putted orginal text

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Flutter Developer", "C# Corner MVP", "Blogger"],
+        strings: ["Developer", "C# Corner MVP", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Flutter Developer", "C# Corner MVP", "Blogger"],
+        strings: ["Developer", "C# Corner MVP", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
